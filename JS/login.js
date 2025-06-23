f2e6934fa008e094a49deee3834734c3cb775c42
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-06-23 23:55:36 +0600

    Extra feature added in login section

--- JS/login.js
+++ JS/login.js
@@ -3,10 +3,13 @@ document.getElementById('login-btn').addEventListener('click', function(){
     const mobileInputFieldNumber = parseFloat(mobileInputField);
     const pinInputField = document.getElementById('pin-input-field').value;
     const pinInputFieldNumber = parseFloat(pinInputField);
-    if(mobileInputFieldNumber === 1713456789 && pinInputFieldNumber === 12345){
+    if(mobileInputFieldNumber === 1713456789 && pinInputFieldNumber === 1234){
         window.location.href = './home.html'
     }else{
         alert("Oops! PIN or number is incorrect.");
     }
+
+    document.getElementById('number-input-field').value='';
+    document.getElementById('pin-input-field').value='';
     
 })
